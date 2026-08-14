a0606208ebb3f39d36ee521529c7f85c2dc83f22
Daniel Allen <dan@aeternagroup.net>
2026-08-15 00:53:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,11 @@ output "redhat_openshift_clusters_id" {
 }
 output "redhat_openshift_clusters_api_server_profile" {
   description = "Map of api_server_profile values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
-  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => v.api_server_profile if v.api_server_profile != null && length(v.api_server_profile) > 0 }
+  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => one(v.api_server_profile) if v.api_server_profile != null && length(v.api_server_profile) > 0 }
 }
 output "redhat_openshift_clusters_cluster_profile" {
   description = "Map of cluster_profile values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
-  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => v.cluster_profile if v.cluster_profile != null && length(v.cluster_profile) > 0 }
+  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => one(v.cluster_profile) if v.cluster_profile != null && length(v.cluster_profile) > 0 }
   sensitive   = true
 }
 output "redhat_openshift_clusters_console_url" {
@@ -17,7 +17,7 @@ output "redhat_openshift_clusters_console_url" {
 }
 output "redhat_openshift_clusters_ingress_profile" {
   description = "Map of ingress_profile values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
-  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => v.ingress_profile if v.ingress_profile != null && length(v.ingress_profile) > 0 }
+  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => one(v.ingress_profile) if v.ingress_profile != null && length(v.ingress_profile) > 0 }
 }
 output "redhat_openshift_clusters_location" {
   description = "Map of location values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
@@ -25,7 +25,7 @@ output "redhat_openshift_clusters_location" {
 }
 output "redhat_openshift_clusters_main_profile" {
   description = "Map of main_profile values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
-  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => v.main_profile if v.main_profile != null && length(v.main_profile) > 0 }
+  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => one(v.main_profile) if v.main_profile != null && length(v.main_profile) > 0 }
 }
 output "redhat_openshift_clusters_name" {
   description = "Map of name values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
@@ -33,7 +33,7 @@ output "redhat_openshift_clusters_name" {
 }
 output "redhat_openshift_clusters_network_profile" {
   description = "Map of network_profile values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
-  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => v.network_profile if v.network_profile != null && length(v.network_profile) > 0 }
+  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => one(v.network_profile) if v.network_profile != null && length(v.network_profile) > 0 }
 }
 output "redhat_openshift_clusters_resource_group_name" {
   description = "Map of resource_group_name values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
@@ -41,7 +41,7 @@ output "redhat_openshift_clusters_resource_group_name" {
 }
 output "redhat_openshift_clusters_service_principal" {
   description = "Map of service_principal values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
-  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => v.service_principal if v.service_principal != null && length(v.service_principal) > 0 }
+  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => one(v.service_principal) if v.service_principal != null && length(v.service_principal) > 0 }
   sensitive   = true
 }
 output "redhat_openshift_clusters_tags" {
@@ -50,6 +50,6 @@ output "redhat_openshift_clusters_tags" {
 }
 output "redhat_openshift_clusters_worker_profile" {
   description = "Map of worker_profile values across all redhat_openshift_clusters, keyed the same as var.redhat_openshift_clusters"
-  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => v.worker_profile if v.worker_profile != null && length(v.worker_profile) > 0 }
+  value       = { for k, v in azurerm_redhat_openshift_cluster.redhat_openshift_clusters : k => one(v.worker_profile) if v.worker_profile != null && length(v.worker_profile) > 0 }
 }
 
